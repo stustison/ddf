e32d8276d27a66fa05c3ff764e208d138e306249
Jacob Beyner <bakejeyner@gmail.com>
2019-07-10 14:29:23 -0700

    Oidc realm refactor (#29)

--- platform/security/security-oidc-realm/src/main/java/org/codice/ddf/security/oidc/realm/CustomOidcProfileCreator.java
+++ platform/security/security-oidc-realm/src/main/java/org/codice/ddf/security/oidc/realm/CustomOidcProfileCreator.java
@@ -18,19 +18,8 @@ import static org.pac4j.core.util.CommonHelper.assertNotNull;
 
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.oauth2.sdk.ParseException;
-import com.nimbusds.oauth2.sdk.http.HTTPRequest;
-import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
-import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
 import com.nimbusds.oauth2.sdk.token.RefreshToken;
-import com.nimbusds.openid.connect.sdk.UserInfoErrorResponse;
-import com.nimbusds.openid.connect.sdk.UserInfoRequest;
-import com.nimbusds.openid.connect.sdk.UserInfoResponse;
-import com.nimbusds.openid.connect.sdk.UserInfoSuccessResponse;
-import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
-import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
-import java.io.IOException;
 import java.util.Map;
 import org.apache.shiro.authc.AuthenticationException;
 import org.pac4j.core.context.WebContext;
@@ -47,74 +36,35 @@ public class CustomOidcProfileCreator<U extends OidcProfile> extends OidcProfile
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CustomOidcProfileCreator.class);
 
-  private OIDCProviderMetadata metadata;
-
-  public CustomOidcProfileCreator(
-      OidcConfiguration configuration, OIDCProviderMetadata oidcProviderMetadata) {
+  public CustomOidcProfileCreator(OidcConfiguration configuration) {
     super(configuration);
-    metadata = oidcProviderMetadata;
   }
 
   @Override
   public U create(OidcCredentials credentials, WebContext context) {
     init();
 
-    final AccessToken accessToken = credentials.getAccessToken();
-    final JWT idToken = credentials.getIdToken();
-
     final U profile = getProfileDefinition().newProfile();
-    profile.setAccessToken(accessToken);
-    profile.setIdTokenString(idToken.getParsedString());
 
-    try {
-      IDTokenClaimsSet claimsSet = new IDTokenClaimsSet(idToken.getJWTClaimsSet());
-      assertNotNull("claimsSet", claimsSet);
-      profile.setId(ProfileHelper.sanitizeIdentifier(profile, claimsSet.getSubject()));
-    } catch (java.text.ParseException | ParseException e) {
-      LOGGER.warn("Unable to extract ID token claim set.", e);
+    final AccessToken accessToken = credentials.getAccessToken();
+    if (accessToken != null && !accessToken.getValue().isEmpty()) {
+      profile.setAccessToken(accessToken);
     }
 
     final RefreshToken refreshToken = credentials.getRefreshToken();
     if (refreshToken != null && !refreshToken.getValue().isEmpty()) {
       profile.setRefreshToken(refreshToken);
-      LOGGER.debug("Refresh Token successful retrieved");
     }
 
+    final JWT idToken = credentials.getIdToken();
+    profile.setIdTokenString(idToken.getParsedString());
+
     try {
-      if (metadata.getUserInfoEndpointURI() != null && accessToken != null) {
+      JWTClaimsSet claimsSet = idToken.getJWTClaimsSet();
+      assertNotNull("claimsSet", claimsSet);
+      profile.setId(ProfileHelper.sanitizeIdentifier(profile, claimsSet.getSubject()));
 
-        final UserInfoRequest userInfoRequest =
-            new UserInfoRequest(metadata.getUserInfoEndpointURI(), (BearerAccessToken) accessToken);
-        final HTTPRequest userInfoHttpRequest = userInfoRequest.toHTTPRequest();
-        userInfoHttpRequest.setConnectTimeout(configuration.getConnectTimeout());
-        userInfoHttpRequest.setReadTimeout(configuration.getReadTimeout());
-        final HTTPResponse httpResponse = userInfoHttpRequest.send();
-        LOGGER.debug(
-            "Token response: status={}, content={}",
-            httpResponse.getStatusCode(),
-            httpResponse.getContent());
-
-        final UserInfoResponse userInfoResponse = UserInfoResponse.parse(httpResponse);
-        if (userInfoResponse instanceof UserInfoErrorResponse) {
-          LOGGER.error(
-              "Bad User Info response, error={}",
-              ((UserInfoErrorResponse) userInfoResponse).getErrorObject());
-        } else {
-          final JWTClaimsSet userInfoClaimsSet;
-          final UserInfoSuccessResponse userInfoSuccessResponse =
-              (UserInfoSuccessResponse) userInfoResponse;
-          if (userInfoSuccessResponse.getUserInfo() != null) {
-            userInfoClaimsSet = userInfoSuccessResponse.getUserInfo().toJWTClaimsSet();
-          } else {
-            userInfoClaimsSet = userInfoSuccessResponse.getUserInfoJWT().getJWTClaimsSet();
-          }
-
-          getProfileDefinition().convertAndAdd(profile, userInfoClaimsSet.getClaims(), null);
-        }
-      }
-
-      for (final Map.Entry<String, Object> entry :
-          idToken.getJWTClaimsSet().getClaims().entrySet()) {
+      for (final Map.Entry<String, Object> entry : claimsSet.getClaims().entrySet()) {
         if (!JwtClaims.SUBJECT.equals(entry.getKey())
             && profile.getAttribute(entry.getKey()) == null) {
           getProfileDefinition()
@@ -123,9 +73,10 @@ public class CustomOidcProfileCreator<U extends OidcProfile> extends OidcProfile
       }
 
       profile.setTokenExpirationAdvance(configuration.getTokenExpirationAdvance());
+
       return profile;
 
-    } catch (final IOException | ParseException | java.text.ParseException e) {
+    } catch (final java.text.ParseException e) {
       throw new AuthenticationException(e);
     }
   }
